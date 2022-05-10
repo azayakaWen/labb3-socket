2af62773a24c25d76f8e08627a335eff0acf3c5f
Emelie Wensberg <emelie.wensberg@iths.se>
2022-05-10 12:18:24 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log(`A client with id ${socket.id} connected to the chat!`);
+
+  socket.on('chatMessage', msg => {
+    io.emit('newChatMessage', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Client ${socket.id} disconnected!`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
